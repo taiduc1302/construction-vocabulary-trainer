ecb7b0ce795198c6914b7d41acedbbb8e8e8ffcf
Mickey Vu <38831891+taiduc1302@users.noreply.github.com>
2026-09-15 11:33:15 -0700

    Register offline service worker

---
+++ src/register-sw.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator){
+  window.addEventListener('load',()=>{
+    navigator.serviceWorker.register('./sw.js').catch(error=>console.warn('Service worker registration failed:',error));
+  });
+}
